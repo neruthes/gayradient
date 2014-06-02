8a58e03ff2c2674a966780cf501737be20b09117
Joy Neop <i@joyneop.com>
2014-06-02 12:28:44 +0800

    test

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// Config
 function wget(url) {
 	var ajax = new XMLHttpRequest();
 	ajax.open("GET", url, false);
@@ -11,12 +10,13 @@ window.gay = {
 gay.templ = '<div class="gayradient" datagay="GAYRADIENT" style="background-image: -webkit-linear-gradient(GAYRADIENT);" onclick="gay.changeBg(THISID)" id=THISID><span>GAYRADIENT</span></div>';
 gay.templMini = '-webkit-linear-gradient(GAYRADIENT)';
 gay.changeBg = function (theGayYouLike) {
-	document.body.style.backgroundImage = document.getElementById(theGayYouLike).style.backgroundImage;
+	document.getElementById("bg").style.backgroundImage = document.getElementById(theGayYouLike).style.backgroundImage;
 };
-gay.allGays = JSON.parse(wget("gay.json"));
 
-for (var i = 0; i < gay.allGays.gay.length; i++) {
-	document.getElementById("content").innerHTML = document.getElementById("content").innerHTML + gay.templ.replace(/GAYRADIENT/g, gay.allGays.gay[i]).replace(/THISID/g, "'gay-id-" + i + "'");
+document.body.onload = function () {
+	gay.allGays = JSON.parse(wget("gay.json"));
+	for (var i = 0; i < gay.allGays.gay.length; i++) {
+		document.getElementById("content").innerHTML = document.getElementById("content").innerHTML + gay.templ.replace(/GAYRADIENT/g, gay.allGays.gay[i]).replace(/THISID/g, "'gay-id-" + i + "'");
+	}
+	document.getElementById("bg").style.backgroundImage = gay.templMini.replace(/GAYRADIENT/g, gay.allGays.gay[0]);
 }
-
-document.body.style.backgroundImage = gay.templMini.replace(/GAYRADIENT/g, gay.allGays.gay[0]);
