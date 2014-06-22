0d59698df644a98aee1cbb9f654c2a959205f38d
Joy Neop <i@joyneop.com>
2014-06-22 13:18:20 +0800

    now testing sharable URL hashtag

--- main.js
+++ main.js
@@ -22,13 +22,14 @@ window.gay = {
 	"author" : "Joy Neop"
 };
 
-gay.templ = '<div class="gayradient" datagay="GAYRADIENT" style="background-image: linear-gradient(GAYRADIENT);" onclick="gay.changeBg(THISID)" id=THISID><span>GAYRADIENT</span></div>';
+gay.templ = '<div class="gayradient" datagay="GAYRADIENT" style="background-image: linear-gradient(GAYRADIENT);" onclick="gay.changeBg(\'THISID\')" id=\'THISID\'><span>GAYRADIENT</span></div>';
 gay.templMini = 'linear-gradient(GAYRADIENT)';
 
 gay.sss = "";
 
-gay.changeBg = function (theGayYouLike) {
-	gay.sss = document.getElementById(theGayYouLike).style.backgroundImage;
+gay.changeBg = function (gayID) {
+	window.location.hash = gayID;
+	gay.sss = document.getElementById(gayID).style.backgroundImage;
 	document.getElementById("body").style.backgroundImage = gay.sss;
 	document.getElementById("bg").style.opacity = "0";
 	setTimeout('document.getElementById("bg").style.backgroundImage = gay.sss;', 200);
@@ -37,11 +38,17 @@ gay.changeBg = function (theGayYouLike) {
 
 gay.allGays = JSON.parse(wget("gay.json"));
 for (var i = 0; i < gay.allGays.gay.length; i++) {
-	document.getElementById("content").innerHTML = document.getElementById("content").innerHTML + gay.templ.replace(/GAYRADIENT/g, gay.allGays.gay[i].toUpperCase()).replace(/THISID/g, "'gay-id-" + i + "'");
+	document.getElementById("content").innerHTML = document.getElementById("content").innerHTML + gay.templ.replace(/GAYRADIENT/g, gay.allGays.gay[i].toUpperCase()).replace(/THISID/g, "gay-id-" + i);
 }
 
-document.getElementById("bg").style.backgroundImage = gay.templMini.replace(/GAYRADIENT/g, gay.allGays.gay[Math.round(Math.random()*(gay.allGays.gay.length-1))]);
-document.getElementById("body").style.backgroundImage = gay.templMini.replace(/GAYRADIENT/g, gay.allGays.gay[Math.round(Math.random()*(gay.allGays.gay.length-1))]);
+if (window.location.href.indexOf("#") != -1) {
+	gay.changeBg(window.location.hash.slice(1));
+	window.scrollTo(0, 0);
+} else {
+	gay.changeBg("gay-id-" + Math.round(Math.random()*(gay.allGays.gay.length-1)));
+	document.getElementById("bg").style.backgroundImage = gay.templMini.replace(/GAYRADIENT/g, gay.allGays.gay[Math.round(Math.random()*(gay.allGays.gay.length-1))]);
+	document.getElementById("body").style.backgroundImage = gay.templMini.replace(/GAYRADIENT/g, gay.allGays.gay[Math.round(Math.random()*(gay.allGays.gay.length-1))]);
+}
 
 // console.log("Hi, I'm seeking 2015 summer internship...");
 // console.log("Intersted in me? Invite me to send an application to your company after reading my resume, thanks! http://www.joyneop.com/resume/ ");
